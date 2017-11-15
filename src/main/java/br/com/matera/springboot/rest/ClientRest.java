a8305ee35c857435d9767b000954bad634b13be4
Claudia <claudia.saft@gmail.com>
2017-11-15 10:35:56 -0200

    add get propertie example

--- src/main/java/br/com/matera/springboot/rest/ClientRest.java
+++ src/main/java/br/com/matera/springboot/rest/ClientRest.java
@@ -7,31 +7,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.matera.springboot.entity.Client;
 import br.com.matera.springboot.repository.ClientRepository;
 
 @RestController
+@RequestMapping(value="/client")
 public class ClientRest {
 
 	@Autowired
 	private ClientRepository clientRepository;
 	
-	@RequestMapping(name="/client", method=RequestMethod.GET)
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public List<Client> getClient() {
 		return (List<Client>) clientRepository.findAll();
 	}
 	
-	@RequestMapping(name="/client", method=RequestMethod.POST)
+	@RequestMapping(value="/", method=RequestMethod.POST)
 	public Long newClient(@RequestBody Client client) {
 		Client newClient = clientRepository.save(client);
 		return newClient.getId();
 	}
 	
-	@RequestMapping("/client/{id}")
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public Client getClientById(@PathVariable("id") Long id) {
 		return clientRepository.findOne(id);
 	}
 	
+	@RequestMapping(value="/by", params = {"id"}, method=RequestMethod.GET)
+	public String getClientByParameterId(@RequestParam Long id) {
+		return clientRepository.findOne(id).getName();
+	}
 }
